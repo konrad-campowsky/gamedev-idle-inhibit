45c79493a21fa428191ee24e521243d6475ac053
Konrad Campowsky <konrad.campowsky@gmail.com>
2025-10-17 20:01:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gamedev-idle-inhibit
+
+Inhibit logind's idle state while a gaming device is connected. 
---
+++ src/idle-inhibit.c
@@ -0,0 +1,92 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <systemd/sd-bus.h>
+
+
+static int acquire_inhibitor_lock(const char *why) {
+    sd_bus *bus = NULL;
+    sd_bus_message *msg = NULL;
+    sd_bus_error error = SD_BUS_ERROR_NULL;
+    int fd = -1, r;
+
+    if ((r = sd_bus_open_system(&bus)) < 0) {
+        fprintf(stderr, "Failed to connect to system bus: %s\n", strerror(-r));
+        return -1;
+    }
+
+    r = sd_bus_call_method(
+        bus,
+        "org.freedesktop.login1",
+        "/org/freedesktop/login1",
+        "org.freedesktop.login1.Manager",
+        "Inhibit",
+        &error,
+        &msg,
+        "ssss",                    // 4 strings:
+        "idle",                    // what
+        "gamepad-monitor",         // who
+         why,                      // why
+        "block"                    // mode
+    );
+
+    if (r < 0) {
+        fprintf(stderr, "Failed to acquire inhibitor lock: %s\n", error.message);
+        sd_bus_error_free(&error);
+        sd_bus_unref(bus);
+        return -1;
+    }
+
+    if ((r = sd_bus_message_read(msg, "h", &fd)) < 0) {
+        fprintf(stderr, "Failed to parse response message: %s\n", strerror(-r));
+    } else {
+        fd = dup(fd);
+    }
+
+    sd_bus_message_unref(msg);
+    sd_bus_unref(bus);
+
+    return fd;
+}
+
+int main(int argc, char* argv[]) {
+    int fd, sig;
+    sigset_t set;
+    const char *why = argc > 1 && argv[1][0] ? argv[1] : "<unknown>";
+
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    sigaddset(&set, SIGTERM);
+
+    if (sigprocmask(SIG_BLOCK, &set, NULL) == -1) {
+        perror("sigprocmask");
+        return 1;
+    }
+
+    if ((fd = acquire_inhibitor_lock(why)) < 0) {
+        return 1;
+    }
+
+    for (;;) {
+        siginfo_t info;
+        if ((sig = sigwaitinfo(&set, &info)) < 0) {
+            if (errno == EINTR) 
+	        continue;
+            perror("sigwaitinfo");
+            break;
+        }
+
+        if (sig == SIGINT || sig == SIGTERM) {
+            printf("received signal %d, shutting down...\n", sig);
+            break;
+        }
+    }
+
+    close(fd);
+
+    return 0;
+}
+
